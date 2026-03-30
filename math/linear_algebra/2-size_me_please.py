6e72e75b980652f30f7593a985dccc68f1f357e5
loicleguen <loic.leguen@hotmail.fr>
2026-03-30 15:32:59 +0200

    add: implement matrix shape calculation in 2-size_me_please.py

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+def matrix_shape(matrix):
+    """Calculates the shape of a matrix
+
+    Args:
+        matrix (list): The matrix to calculate the shape of
+
+    Returns:
+        list: The shape of the matrix
+    """
+    if type(matrix) is not list or len(matrix) == 0:
+        return []
+    if type(matrix[0]) is not list:
+        return [len(matrix)]
+    return [len(matrix)] + matrix_shape(matrix[0])

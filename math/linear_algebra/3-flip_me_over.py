54ee2af862de15fad264ed809a24350b03717632
loicleguen <loic.leguen@hotmail.fr>
2026-03-30 15:52:18 +0200

    add: restore module docstring in 3-flip_me_over.py

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""Defines the function matrix_transpose(matrix)"""
+
+
 def matrix_transpose(matrix):
     """Retourne la transposée d'une matrice 2D"""
     return [[row[i] for row in matrix] for i in range(len(matrix[0]))]

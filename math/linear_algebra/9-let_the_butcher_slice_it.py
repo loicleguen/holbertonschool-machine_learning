9ad0d482f147a3fb9b702331b17cfbcd65a891d5
loicleguen <loic.leguen@hotmail.fr>
2026-04-01 12:07:51 +0200

    remove: delete docstring describing matrix slicing functionality

--- math/linear_algebra/9-let_the_butcher_slice_it.py
+++ math/linear_algebra/9-let_the_butcher_slice_it.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-"""Show the middle two rows, middle two columns,
-and bottom-right square 3x3 matrix of a given 4x6 matrix."""
 import numpy as np
 matrix = np.array([[1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12],
                    [13, 14, 15, 16, 17, 18], [19, 20, 21, 22, 23, 24]])

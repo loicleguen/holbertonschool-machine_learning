f5ba537f5ff27186f408febd0d3bff0216216fec
loicleguen <loic.leguen@hotmail.fr>
2026-04-01 13:43:32 +0200

    add: implement np_elementwise function for element-wise operations on matrices

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Bracin the elements"""
+
+
+def np_elementwise(mat1, mat2):
+    """Performs element-wise addition, subtraction,
+    multiplication, and division"""
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)

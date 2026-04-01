2f5b075b5ebf56d6d25f3faa226088bb8f69feac
loicleguen <loic.leguen@hotmail.fr>
2026-04-01 13:55:10 +0200

    add: implement np_matmul function for matrix multiplication

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module that contains the function np_matmul"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Function that performs matrix multiplication
+    between two matrices using"""
+    return np.matmul(mat1, mat2)

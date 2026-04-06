447097d7de6a72bec955ae66d8ffac073ad17e46
loicleguen <loic.leguen@hotmail.fr>
2026-04-06 18:13:38 +0200

    fix: correct variable name for fruit labels in stacked bar chart

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -9,13 +9,19 @@ def bars():
     np.random.seed(5)
     fruit = np.random.randint(0, 20, (4, 3))
     plt.figure(figsize=(6.4, 4.8))
+
     people = ['Farrah', 'Fred', 'Felicia']
-    fuits = ['apples', 'bananas', 'oranges', 'peaches']
     colors = ['r', 'y', '#ff8000', '#ffe5b4']
+    labels = ['apples', 'bananas', 'oranges', 'peaches']
+
     bottom = np.zeros(3)
     for i in range(4):
-        plt.bar(people, fruit[i], width=0.5, bottom=bottom, color=colors[i], label=fuits[i])
+        plt.bar(
+            people, fruit[i], width=0.5, bottom=bottom,
+            color=colors[i], label=labels[i]
+            )
         bottom += fruit[i]
+
     plt.ylabel('Quantity of Fruit')
     plt.ylim(0, 80)
     plt.yticks(np.arange(0, 81, 10))

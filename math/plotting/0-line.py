186dad7060488f970ab6037f2b2608f609beea34
loicleguen <loic.leguen@hotmail.fr>
2026-04-06 15:16:33 +0200

    fix: update line style in plot function to solid line

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -8,6 +8,6 @@ def line():
     """Draw a line plot of y = x^3 for x in the range [0, 10]."""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
-    plt.plot(np.arange(0, 11), y, 'r')
+    plt.plot(np.arange(0, 11), y, 'r-')
     plt.xlim(0, 10)
     plt.show()

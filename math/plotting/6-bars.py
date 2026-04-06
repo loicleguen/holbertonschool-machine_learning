c8d88f7f82e23e88203ca43ae4aed7afdf7aeedb
loicleguen <loic.leguen@hotmail.fr>
2026-04-06 18:09:06 +0200

    refactor: clean up whitespace in bar plotting function

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -9,16 +9,13 @@ def bars():
     np.random.seed(5)
     fruit = np.random.randint(0, 20, (4, 3))
     plt.figure(figsize=(6.4, 4.8))
-
     people = ['Farrah', 'Fred', 'Felicia']
     fuits = ['apples', 'bananas', 'oranges', 'peaches']
     colors = ['r', 'y', '#ff8000', '#ffe5b4']
-
     bottom = np.zeros(3)
     for i in range(4):
         plt.bar(people, fruit[i], width=0.5, bottom=bottom, color=colors[i], label=fuits[i])
         bottom += fruit[i]
-
     plt.ylabel('Quantity of Fruit')
     plt.ylim(0, 80)
     plt.yticks(np.arange(0, 81, 10))

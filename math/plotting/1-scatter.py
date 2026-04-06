4d27589413f41d80746ca17ea35b7269a9293009
loicleguen <loic.leguen@hotmail.fr>
2026-04-06 15:28:37 +0200

    feat: add scatter plot function to visualize height and weight data

---
+++ math/plotting/1-scatter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Scatter plot of height and weight for 2000 individuals"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def scatter():
+    """Scatter plot of height and weight for 2000 individuals"""
+    mean = [69, 0]
+    cov = [[15, 8], [8, 15]]
+    np.random.seed(5)
+    x, y = np.random.multivariate_normal(mean, cov, 2000).T
+    y += 180
+    plt.figure(figsize=(6.4, 4.8))
+    plt.scatter(x, y, c='m', s=10)
+    plt.xlabel('Height (in)')
+    plt.ylabel('Weight (lbs)')
+    plt.title('Men\'s Height vs Weight')
+    plt.show()

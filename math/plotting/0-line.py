4a0584dd5c20b1d1c298a2a09799c7af2ad37559
loicleguen <loic.leguen@hotmail.fr>
2026-04-06 15:04:55 +0200

    feat: add line plotting function to visualize y = x^3

---
+++ math/plotting/0-line.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+def line():
+    """Draw a line plot of y = x^3 for x in the range [0, 10]."""
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+    plt.plot(np.arange(0, 11), y, 'r')
+    plt.xlim(0, 10)
+    plt.show()

7a7e3bd3088eddb2aaf472d16a0917939d5d104a
loicleguen <loic.leguen@hotmail.fr>
2026-04-07 11:57:41 +0200

    fix: improve poly_derivative function to handle empty list case

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -4,11 +4,8 @@
 
 def poly_derivative(poly):
     """Calculates the derivative of a polynomial:"""
-    if not isinstance(poly, list):
+    if not isinstance(poly, list) or len(poly) == 0:
         return None
     if len(poly) == 1:
         return [0]
-    derivative = [i * poly[i] for i in range(1, len(poly))]
-    if derivative == 0:
-        return [0]
-    return derivative
+    return [i * poly[i] for i in range(1, len(poly))]

1d1684f3d20773603318eaad5837cd877e3b353d
loicleguen <loic.leguen@hotmail.fr>
2026-04-07 11:11:14 +0200

    fix: optimize summation_i_squared function for calculating sum of squares

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -5,4 +5,4 @@ def summation_i_squared(n):
     """Function that calculates the sum of i squared from 1 to n"""
     if not isinstance(n, int) or n < 1:
         return None
-    return sum(i ** 2 for i in range(1, n + 1))
+    return n * (n + 1) * (2 * n + 1) // 6

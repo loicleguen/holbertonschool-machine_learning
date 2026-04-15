bf0e3fb238d4aca2856f5dd06ff08efa46469049
loicleguen <loic.leguen@hotmail.fr>
2026-04-15 10:21:29 +0200

    fix: add missing class docstring for Isolation Random Tree

--- supervised_learning/decision_tree/10-isolation_tree.py
+++ supervised_learning/decision_tree/10-isolation_tree.py
@@ -6,6 +6,7 @@ Leaf = __import__('8-build_decision_tree').Leaf
 
 
 class Isolation_Random_Tree():
+    """Class that represents an isolation tree"""
     def __init__(self, max_depth=10, seed=0, root=None):
         """Class that represents an isolation tree"""
         self.rng = np.random.default_rng(seed)

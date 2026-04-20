a24eec02ffb74bad5f36a68ff3a8ae19f693fc74
loicleguen <loic.leguen@hotmail.fr>
2026-04-20 14:45:17 +0200

    feat: add Neuron class for binary classification and data visualization scripts

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import numpy as np
+"""Defines a single neuron performing binary classification"""
+
+
+class Neuron:
+	"""Class Neuron that defines a single neuron performing binary classification"""
+	def __init__(self, nx):
+		"""Constructor method for the Neuron class"""
+		if not isinstance(nx, int):
+			raise TypeError("nx must be an integer")
+		if nx < 1:
+			raise ValueError("nx must be a positive integer")
+
+		self.W = np.random.randn(1, nx)
+		self.b = 0
+		self.A = 0

9a13d5fd72b1ee246777026bd2c3ab99f3ae070b
loicleguen <loic.leguen@hotmail.fr>
2026-04-22 14:15:55 +0200

    feat: implement DeepNeuralNetwork class with initialization, forward propagation, cost calculation, evaluation, and gradient descent

---
+++ supervised_learning/classification/21-deep_neural_network.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+"""Defines a deep neural network performing binary classification"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """Deep Neural Network performing binary classification"""
+
+    def __init__(self, nx, layers):
+        """Constructor method for DeepNeuralNetwork class"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(layers, list) or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+
+        for i in range(self.__L):
+            if not isinstance(layers[i], int) or layers[i] <= 0:
+                raise TypeError("layers must be a list of positive integers")
+
+            if i == 0:
+                prev_nodes = nx
+            else:
+                prev_nodes = layers[i - 1]
+
+            current_nodes = layers[i]
+
+            w_key = "W{}".format(i + 1)
+            b_key = "b{}".format(i + 1)
+
+            he_factor = np.sqrt(2 / prev_nodes)
+            self.__weights[w_key] = np.random.randn(
+                current_nodes, prev_nodes) * he_factor
+            self.__weights[b_key] = np.zeros((current_nodes, 1))
+
+    @property
+    def L(self):
+        """Getter method for L attribute"""
+        return self.__L
+
+    @property
+    def cache(self):
+        """Getter method for cache attribute"""
+        return self.__cache
+
+    @property
+    def weights(self):
+        """Getter method for weights attribute"""
+        return self.__weights
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neural network"""
+        self.__cache["A0"] = X
+
+        for i in range(1, self.__L + 1):
+            w_key = "W{}".format(i)
+            b_key = "b{}".format(i)
+            a_prev_key = "A{}".format(i - 1)
+            a_key = "A{}".format(i)
+            z = np.matmul(self.__weights[w_key],
+                          self.__cache[a_prev_key]) + self.__weights[b_key]
+            self.__cache[a_key] = 1 / (1 + np.exp(-z))
+        return self.__cache[a_key], self.__cache
+
+    def cost(self, Y, A):
+        """Calculates the cost of the model using logistic regression"""
+        m = Y.shape[1]
+        cost = -np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)) / m
+        return cost
+
+    def evaluate(self, X, Y):
+        """Evaluates the neural network’s predictions"""
+        A, _ = self.forward_prop(X)
+        predictions = np.where(A >= 0.5, 1, 0)
+        cost = self.cost(Y, A)
+        return predictions, cost
+
+    def gradient_descent(self, Y, cache, alpha=0.05):
+        """Calculates one pass of gradient descent on the neural network"""
+        m = Y.shape[1]
+        dZ = cache["A{}".format(self.__L)] - Y
+
+        for i in range(self.__L, 0, -1):
+            w_key = "W{}".format(i)
+            b_key = "b{}".format(i)
+            A_prev_key = "A{}".format(i - 1)
+
+            dW = np.matmul(dZ, cache[A_prev_key].T) / m
+            db = np.sum(dZ, axis=1, keepdims=True) / m
+
+            if i > 1:
+                dA = np.matmul(self.__weights[w_key].T, dZ)
+                dZ = dA * (cache[A_prev_key] * (1 - cache[A_prev_key]))
+
+            self.__weights[w_key] -= alpha * dW
+            self.__weights[b_key] -= alpha * db

4e07098b5a46aa48f06768f7eb3f9af7ead774a6
loicleguen <loic.leguen@hotmail.fr>
2026-04-23 15:50:29 +0200

    refactor: update main script to use Neuron class and load binary training data

--- supervised_learning/classification/0-main.py
+++ supervised_learning/classification/0-main.py
@@ -2,27 +2,17 @@
 
 import numpy as np
 
-Deep = __import__('27-deep_neural_network').DeepNeuralNetwork
-one_hot_encode = __import__('24-one_hot_encode').one_hot_encode
+Neuron = __import__('0-neuron').Neuron
 
-lib = np.load('../data/MNIST.npz')
-X_train_3D = lib['X_train']
-Y_train = lib['Y_train']
-X_train = X_train_3D.reshape((X_train_3D.shape[0], -1)).T
-Y_train_one_hot = one_hot_encode(Y_train, 10)
+lib_train = np.load('../data/Binary_Train.npz')
+X_3D, Y = lib_train['X'], lib_train['Y']
+X = X_3D.reshape((X_3D.shape[0], -1)).T
 
-# Crée un nouveau modèle (poids aléatoires)
-deep = Deep(784, [128, 64, 10])
-
-# Affiche zéros placeholder
-print(np.zeros((10, X_train.shape[1])))
-
-A, cost = deep.evaluate(X_train, Y_train_one_hot)
-print(cost)
-print(deep.L)
-for i in range(deep.L + 1):
-    if i == 0:
-        print("A{}".format(i), deep.cache.get("A{}".format(i)))
-    else:
-        print("W{}".format(i), deep.weights["W{}".format(i)])
-        print("A{}".format(i), deep.cache.get("A{}".format(i)))
+np.random.seed(0)
+neuron = Neuron(X.shape[0])
+print(neuron.W)
+print(neuron.W.shape)
+print(neuron.b)
+print(neuron.A)
+neuron.A = 10
+print(neuron.A)

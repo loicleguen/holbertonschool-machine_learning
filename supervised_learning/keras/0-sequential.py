9853a5abd0d20bf1a5be2e95c0aaf6e9488c2fb5
loicleguen <loic.leguen@hotmail.fr>
2026-04-27 09:32:08 +0200

    feat: implement build_model function for Keras Sequential API

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -3,4 +3,17 @@
 import tensorflow.keras as K
 
 
-def build_model
+def build_model(nx, layers, activations, lambtha, kepp_prob):
+    """Builds a neural network with the Keras Sequential API"""
+    model = K.Sequential()
+    for i in range(len(layers)):
+        model.add(K.layers.Dense(
+            layers[i],
+            activation=activations[i],
+            kernel_regularizer=K.regularizers.l2(lambtha),
+            input_shape=(nx,) if i == 0 else None
+        ))
+
+        if i != len(layers) - 1:
+            model.add(K.layers.Dropout(1 - kepp_prob))
+    return model

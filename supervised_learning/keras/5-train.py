63b7f2517919b140df9cc5f2bf09aeffa74cba5a
loicleguen <loic.leguen@hotmail.fr>
2026-04-27 10:52:49 +0200

    feat: implement train_model function for mini-batch gradient descent training

---
+++ supervised_learning/keras/5-train.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Trains a model using mini-batch gradient descent with validation data"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, verbose=True, shuffle=False):
+    """Trains a model using mini-batch gradient
+    descent with optional validation data"""
+    history = network.fit(
+        data,
+        labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        shuffle=shuffle,
+        validation_data=validation_data
+    )
+    return history

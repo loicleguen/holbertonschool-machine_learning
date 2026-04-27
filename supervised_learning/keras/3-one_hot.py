4882633f16a39091f50218ac5027c3d38ba3ce63
loicleguen <loic.leguen@hotmail.fr>
2026-04-27 10:37:50 +0200

    feat: refactor one_hot function to use Keras utility for one-hot encoding

--- supervised_learning/keras/3-one_hot.py
+++ supervised_learning/keras/3-one_hot.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 """Converts a label vector into a one-hot matrix"""
-import numpy as np
+import tensorflow.keras as K
 
 
 def one_hot(labels, classes=None):
     """Converts a label vector into a one-hot matrix"""
-    if classes is None:
-        classes = np.max(labels) + 1
-    return np.eye(classes)[labels]
+    return K.utils.to_categorical(labels, num_classes=classes)

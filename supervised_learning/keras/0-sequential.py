497f251e3b60dda468d86f4f7317d898d2f8ef28
loicleguen <loic.leguen@hotmail.fr>
2026-04-27 09:54:32 +0200

    fix: adjust input_shape handling in build_model function for Keras Sequential API

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -7,13 +7,19 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     """Builds a neural network with the Keras Sequential API"""
     model = K.Sequential()
     for i in range(len(layers)):
-        model.add(K.layers.Dense(
-            layers[i],
-            activation=activations[i],
-            kernel_regularizer=K.regularizers.l2(lambtha),
-            input_shape=(nx,) if i == 0 else None
-        ))
-
-        if i != len(layers) - 1:
+        if i == 0:
+            model.add(K.layers.Dense(
+                layers[i],
+                activation=activations[i],
+                kernel_regularizer=K.regularizers.l2(lambtha),
+                input_shape=(nx,)
+            ))
+        else:
+            model.add(K.layers.Dense(
+                layers[i],
+                activation=activations[i],
+                kernel_regularizer=K.regularizers.l2(lambtha)
+            ))
+        if i < len(layers) - 1:
             model.add(K.layers.Dropout(1 - keep_prob))
     return model

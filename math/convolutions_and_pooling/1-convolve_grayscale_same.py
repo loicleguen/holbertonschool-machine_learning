8cfcc8f0155d0a2f3e23a13e4ce7fd99656173b9
loicleguen <loic.leguen@hotmail.fr>
2026-05-19 14:47:48 +0200

    fix: improve docstring and variable naming in same convolution function

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -1,35 +1,37 @@
 #!/usr/bin/env python3
-"""Convolution with same padding on grayscale images."""
+"""write a function that performs a same convolution on grayscale images"""
 import numpy as np
 
 
 def convolve_grayscale_same(images, kernel):
-    """
-    images: numpy.ndarray of shape (m, h, w)
-    kernel: numpy.ndarray of shape (kh, kw)
-    Retourne: numpy.ndarray of shape (m, h, w)
+    """performs a same convolution on grayscale images
+
+    Args:
+        images: numpy.ndarray with shape (m, h, w)
+        kernel: numpy.ndarray with shape (kh, kw)
+
+    Returns:
+        numpy.ndarray containing the convolved images
     """
     m, h, w = images.shape
     kh, kw = kernel.shape
 
     pad_top = (kh - 1) // 2
-    pad_bottom = (kh - 1) - pad_top
+    pad_bottom = kh // 2
     pad_left = (kw - 1) // 2
-    pad_right = (kw - 1) - pad_left
+    pad_right = kw // 2
 
-    images_padded = np.pad(images,
-                           ((0, 0),
-                            (pad_top, pad_bottom),
-                            (pad_left, pad_right)
-                            ),
-                           mode='constant', constant_values=0)
+    padded_images = np.pad(
+        images,
+        ((0, 0), (pad_top, pad_bottom), (pad_left, pad_right)),
+        mode='constant'
+    )
 
-    out_h, out_w = h, w
-    output = np.zeros((m, out_h, out_w))
+    convolved_images = np.zeros((m, h, w))
 
-    for i in range(out_h):
-        for j in range(out_w):
-            patch = images_padded[:, i:i + kh, j:j + kw]
-            output[:, i, j] = np.sum(patch * kernel, axis=(1, 2))
+    for i in range(h):
+        for j in range(w):
+            patch = padded_images[:, i:i + kh, j:j + kw]
+            convolved_images[:, i, j] = np.sum(patch * kernel, axis=(1, 2))
 
-    return output
+    return convolved_images

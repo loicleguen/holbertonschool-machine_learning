20e7dfb18800bb7ba38cb7acdb932769a4f8aef0
loicleguen <loic.leguen@hotmail.fr>
2026-05-19 14:57:43 +0200

    fix: enhance docstring clarity and simplify padding logic in same convolution function

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -7,31 +7,27 @@ def convolve_grayscale_same(images, kernel):
     """performs a same convolution on grayscale images
 
     Args:
-        images: numpy.ndarray with shape (m, h, w)
-        kernel: numpy.ndarray with shape (kh, kw)
-
+        images: numpy.ndarray with shape (m, h, w) containing multiple
+                grayscale images
+            m: number of images
+            h: height in pixels of the images
+            w: width in pixels of the images
+        kernel: numpy.ndarray with shape (kh, kw) containing the kernel for
+                the convolution
+            kh: height of the kernel
+            kw: width of the kernel
     Returns:
         numpy.ndarray containing the convolved images
     """
     m, h, w = images.shape
     kh, kw = kernel.shape
-
-    pad_top = (kh - 1) // 2
-    pad_bottom = kh // 2
-    pad_left = (kw - 1) // 2
-    pad_right = kw // 2
-
-    padded_images = np.pad(
-        images,
-        ((0, 0), (pad_top, pad_bottom), (pad_left, pad_right)),
-        mode='constant'
-    )
-
+    pad_h = kh // 2
+    pad_w = kw // 2
+    padded_images = np.pad(images, ((0, 0), (pad_h, pad_h), (pad_w, pad_w)),
+                           mode='constant')
     convolved_images = np.zeros((m, h, w))
-
     for i in range(h):
         for j in range(w):
-            patch = padded_images[:, i:i + kh, j:j + kw]
-            convolved_images[:, i, j] = np.sum(patch * kernel, axis=(1, 2))
-
+            convolved_images[:, i, j] = np.sum(
+                padded_images[:, i:i + kh, j:j + kw] * kernel, axis=(1, 2))
     return convolved_images

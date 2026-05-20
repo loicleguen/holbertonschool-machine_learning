b1dd510f193999bbcc942da5cc75cd4717ba60f2
loicleguen <loic.leguen@hotmail.fr>
2026-05-20 11:11:56 +0200

    feat: implement grayscale convolution function with customizable padding and stride

---
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""Convolution sur images en niveaux de gris avec padding et stride."""
+import numpy as np
+
+
+def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
+    """
+    Performs a convolution on grayscale images.
+
+    Args:
+        images: numpy.ndarray of shape (m, h, w)
+        kernel: numpy.ndarray of shape (kh, kw)
+        padding: 'same', 'valid', or tuple (ph, pw)
+        stride: tuple (sh, sw)
+
+    Returns:
+        numpy.ndarray containing the convolved images
+    """
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+    sh, sw = stride
+
+    if padding == 'same':
+        out_h = (h + sh - 1) // sh
+        out_w = (w + sw - 1) // sw
+        pad_h = max((out_h - 1) * sh + kh - h, 0)
+        pad_w = max((out_w - 1) * sw + kw - w, 0)
+        pad_top = pad_h // 2
+        pad_bottom = pad_h - pad_top
+        pad_left = pad_w // 2
+        pad_right = pad_w - pad_left
+    elif padding == 'valid':
+        pad_top = pad_bottom = pad_left = pad_right = 0
+    else:
+        pad_top = pad_bottom = padding[0]
+        pad_left = pad_right = padding[1]
+
+    padded_images = np.pad(
+        images,
+        ((0, 0), (pad_top, pad_bottom), (pad_left, pad_right)),
+        mode='constant'
+    )
+
+    ph = padded_images.shape[1]
+    pw = padded_images.shape[2]
+    out_h = ((ph - kh) // sh) + 1
+    out_w = ((pw - kw) // sw) + 1
+
+    convolved_images = np.zeros((m, out_h, out_w))
+
+    for i in range(out_h):
+        for j in range(out_w):
+            convolved_images[:, i, j] = np.sum(
+                padded_images[
+                    :, i * sh:i * sh + kh, j * sw:j * sw + kw] * kernel,
+                axis=(1, 2)
+            )
+
+    return convolved_images

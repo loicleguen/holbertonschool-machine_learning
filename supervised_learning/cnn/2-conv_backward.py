fix: correct padding calculation in conv_backward function

supervised_learning/cnn/2-conv_backward.py
@@ -21,13 +21,11 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
     sh, sw = stride
 
     if padding == "same":
-        ph = ((h_prev - 1) * sh + kh - h_prev) // 2
-        pw = ((w_prev - 1) * sw + kw - w_prev) // 2
-    elif padding == "valid":
+        ph = ((h_prev - 1) * sh + kh - h_prev) // 2 + 1
+        pw = ((w_prev - 1) * sw + kw - w_prev) // 2 + 1
+    else:
         ph = 0
         pw = 0
-    else:
-        raise ValueError("padding must be 'same' or 'valid'")
 
     A_prev_pad = np.pad(
         A_prev,
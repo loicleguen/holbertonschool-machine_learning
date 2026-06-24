13df7e43c7f4cb741acbce104df4004452bb27ca
loicleguen <loic.leguen@hotmail.fr>
2026-06-24 10:39:37 +0200

    add task 9

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -78,3 +78,26 @@ class Normal:
         exponent = -0.5 * (((x - self.mean) / self.stddev) ** 2)
 
         return coefficient * (e ** exponent)
+
+    def cdf(self, x):
+        """
+        Calcule la valeur de la CDF pour une valeur x donnée.
+
+        Paramètres:
+            x (int/float): La valeur à évaluer.
+
+        Retourne:
+            float: La valeur de la CDF pour x.
+        """
+        pi = 3.1415926536
+
+        # 1. Calcul de l'argument 'z' pour la fonction erf
+        z = (x - self.mean) / (self.stddev * (2 ** 0.5))
+
+        # 2. Approximation de erf(z) jusqu'à la puissance 9
+        erf = (2 / (pi ** 0.5)) * (
+            z - (z ** 3) / 3 + (z ** 5) / 10 - (z ** 7) / 42 + (z ** 9) / 216
+        )
+
+        # 3. Application de la formule globale de la CDF normale
+        return 0.5 * (1 + erf)

a20f1a394bcb98ae2254e791ed2e27d93699aa8a
loicleguen <loic.leguen@hotmail.fr>
2026-06-24 10:33:52 +0200

    add task 8

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -60,3 +60,21 @@ class Normal:
             float: La valeur x de z.
         """
         return self.mean + (z * self.stddev)
+
+    def pdf(self, x):
+        """
+        Calcule la valeur de la PDF pour une valeur x donnée.
+
+        Paramètres:
+            x (int/float): La valeur à évaluer.
+
+        Retourne:
+            float: La valeur de la PDF pour x.
+        """
+        pi = 3.1415926536
+        e = 2.7182818285
+
+        coefficient = 1 / (self.stddev * ((2 * pi) ** 0.5))
+        exponent = -0.5 * (((x - self.mean) / self.stddev) ** 2)
+
+        return coefficient * (e ** exponent)

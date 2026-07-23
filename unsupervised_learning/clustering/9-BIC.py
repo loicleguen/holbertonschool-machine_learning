049604a3ccbaf1071a33a6f82ac5d2e94c6d919e
loicleguen <loic.leguen@hotmail.fr>
2026-07-23 11:12:42 +0200

    fix: improve input validation in BIC function

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -28,17 +28,21 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     """
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None, None
-    if not isinstance(kmin, int) or kmin <= 0:
-        return None, None, None, None
 
     n, d = X.shape
 
+    if not isinstance(kmin, int) or kmin <= 0 or kmin > n:
+        return None, None, None, None
+
     if kmax is None:
         kmax = n
-    if not isinstance(kmax, int) or kmax <= 0:
+
+    if not isinstance(kmax, int) or kmax <= 0 or kmax > n:
         return None, None, None, None
-    if kmin > kmax or kmax > n:
+
+    if kmin > kmax:
         return None, None, None, None
+
     if not isinstance(iterations, int) or iterations <= 0:
         return None, None, None, None
     if not isinstance(tol, (int, float)) or tol < 0:

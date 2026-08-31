5f42dbb47fc835714f83cc4df8de6848e0190ea6
loicleguen <loic.leguen@hotmail.fr>
2026-08-31 15:42:25 +0200

    fix: correct argument usage for epochs in Word2Vec model training

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -27,11 +27,13 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
     sg = 0 if cbow else 1
 
     model = gensim.models.Word2Vec(
+        sentences=sentences,
         vector_size=vector_size,
         min_count=min_count,
         window=window,
         negative=negative,
         sg=sg,
+        epochs=epochs,
         seed=seed,
         workers=workers
     )
@@ -40,7 +42,7 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
     model.train(
         sentences,
         total_examples=model.corpus_count,
-        epochs=epochs
+        epochs=model.epochs
     )
 
     return model

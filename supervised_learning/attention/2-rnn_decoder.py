a4dbb651d5bbfca1e50207f414b0049e22c9d96a
loicleguen <loic.leguen@hotmail.fr>
2026-09-09 15:59:03 +0200

    feat: implement RNNDecoder class for automatic translation with attention mechanism

---
+++ supervised_learning/attention/2-rnn_decoder.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""
+Module RNNDecoder pour le décodeur d'un modèle de traduction automatique
+"""
+import tensorflow as tf
+SelfAttention = __import__('1-self_attention').SelfAttention
+
+
+class RNNDecoder(tf.keras.layers.Layer):
+    """
+    Décode les séquences d'entrée avec un mécanisme d'attention et un GRU
+    """
+
+    def __init__(self, vocab, embedding, units, batch):
+        """
+        Constructeur de la classe
+
+        Args:
+            vocab: entier, taille du vocabulaire de sortie
+            embedding: entier, dimension du vecteur d'embedding
+            units: entier, nombre d'unités cachées dans la cellule RNN
+            batch: entier, taille du batch
+        """
+        super(RNNDecoder, self).__init__()
+        self.embedding = tf.keras.layers.Embedding(vocab, embedding)
+        self.gru = tf.keras.layers.GRU(
+            units,
+            return_sequences=True,
+            return_state=True,
+            recurrent_initializer='glorot_uniform'
+        )
+        self.F = tf.keras.layers.Dense(vocab)
+        self.attention = SelfAttention(units)
+
+    def call(self, x, s_prev, hidden_states):
+        """
+        Passe les entrées dans la couche du décodeur
+
+        Args:
+            x: tensor (batch, 1) index du mot précédent
+            s_prev: tensor (batch, units) état caché précédent
+            hidden_states: tensor (batch, input_seq_len, units) sorties
+                           de l'encodeur
+
+        Returns:
+            y, s
+            - y: tensor (batch, vocab) vecteur pour le mot prédit
+            - s: tensor (batch, units) nouvel état caché du décodeur
+        """
+        context, _ = self.attention(s_prev, hidden_states)
+        x = self.embedding(x)
+        context_expanded = tf.expand_dims(context, axis=1)
+
+        x = tf.concat([context_expanded, x], axis=-1)
+
+        output, s = self.gru(x, initial_state=s_prev)
+        output = tf.reshape(output, (-1, output.shape[2]))
+
+        y = self.F(output)
+
+        return y, s
